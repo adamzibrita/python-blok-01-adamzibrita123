f701786d01d2905344498b05a04ae5b3723464e1
adamzibrita123 <adzibri@gymcheb.cz>
2026-03-17 08:43:13 +0100

    Update main.py

--- Lekce_02_Input/main.py
+++ Lekce_02_Input/main.py
@@ -2,15 +2,23 @@
 # ÚLOHA 1: Registrace (jméno a město)
 # ==========================================
 # Sem napiš svůj kód:
-
+jmeno = input("Zadej své jméno: ")
+mesto = input("Zadej město, ve kterém bydlíš: ")
+print (f"Těší mě {jmeno}, bydlíš ve skvělém městě s názvem {mesto}.")
 
 # ==========================================
 # ÚLOHA 2: Jednoduchá kalkulačka (součet)
 # ==========================================
 # Sem napiš svůj kód:
 
+x = int(input("Zadej 1. číslo: "))
+y = int(input("Zadej 2. číslo: "))
+print (f"Výsledek je: {x+y}")
 
 # ==========================================
 # ÚLOHA 3: Výpočet psího věku (věk * 7)
 # ==========================================
 # Sem napiš svůj kód:
+clovekVek = int(input("Zadej svůj věk: "))
+pesVek = clovekVek * 7
+print(f"Váš psí věk je: {pesVek}")
